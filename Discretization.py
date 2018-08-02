337f8fb4274e7376dce5f7d3f5d8ebf04cbc8815
Kirsten Thobe <kirsten.thobe@gmail.com>
2018-08-02 17:51:50 +0200

    remove headers todo

--- Discretization.py
+++ Discretization.py
@@ -1,19 +1,25 @@
 # Discretization of Bioplex data
 
 import csv
+import numpy as np
+
 
 with open('Bp1257DMSO.csv') as csvfile:
-        data = csv.reader(csvfile, delimiter=',')
+        data_raw = csv.reader(csvfile, delimiter=',')
         # remove headers
+        data = data_raw[2-6,2-12]
+        print(data)
         #Bp1257DMSO = data[]
-        x=[]
-        for row in data:
-            x.append(row[1])
-            print(x)
+        # x=[]
+        # for row in data:
+        #     x.append(row[1])
+        #     print(x)
 
-# mean_array = np.array([])
-# median_array = np.array([])
-#
-# for line in Bp1851exp1:
-#     result = do_stuff(line)
-#     result_array = np.append(result_array, result)
+        # mean_array = np.array([])
+        # median_array = np.array([])
+        #
+        # for row in data:
+        #     print(row)
+        #     result = np.mean(row)
+            # mean_array = np.append(result_array, result)
+            # print(result_array)
